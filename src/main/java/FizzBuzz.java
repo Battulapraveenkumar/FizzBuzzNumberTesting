2917bbd46c9d9d403676f5baab7bf7472e695f70
Battulapraveenkumar <praveenkumarbattula8@gmail.com>
2021-08-19 20:35:53 +0530

    Requirement 1: Divisible by Three print(Fizz) and Divisible by Five(Buzz)

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,2 +1,17 @@
 public class FizzBuzz {
+    int number;
+    public FizzBuzz(int n) {
+        this.number=n;
+    }
+
+    public String fizzbuzz() {
+
+            if(number%3==0)
+                return  "Fizz";
+            if(number%5==0)
+                return "Buzz";
+            else
+                return null;
+
+    }
 }
--- src/test/java/FizzBuzzTesting.java
+++ src/test/java/FizzBuzzTesting.java
@@ -1,2 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTesting {
+    @Test
+    public void toReturnFizzIfANumberIsDivisibleByThree()
+    {
+        FizzBuzz obj=new FizzBuzz(6);
+        String expectedOutput="Fizz";
+        String actualOutput=obj.fizzbuzz();
+        assertEquals(expectedOutput,actualOutput);
+    }
+
+    @Test
+    public void toReturnBuzzIfANumberIsDivisibleByFive()
+    {
+        FizzBuzz obj=new FizzBuzz(10);
+        String expectedOutput="Buzz";
+        String actualOutput=obj.fizzbuzz();
+        assertEquals(expectedOutput,actualOutput);
+    }
+    @Test
+    public void toReturnNullIfANumberIsNegative()
+    {
+        FizzBuzz obj=new FizzBuzz(7);
+        String expectedOutput=null;
+        String actualOutput=obj.fizzbuzz();
+        assertEquals(null,actualOutput);
+    }
 }
